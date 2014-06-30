e59966ed9c671adab5005c078c6d0942e5e325fa
zzz6519003 <zhengzhongzhao@gmail.com>
2014-06-30 16:36:27 +0800

    it won't crash at least

---
+++ Paper/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Paper
+//
+//  Created by Snowmanzzz on 6/30/14.
+//  Copyright (c) 2014 Heberti Almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
